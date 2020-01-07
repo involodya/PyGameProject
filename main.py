3527044d0d6ed2116a028dcc28dcdc5f88d60d95
vovans32 <kpvcha4@yandex.ru>
2020-01-07 12:52:12 +0500

    1 normal comm

--- main.py
+++ main.py
@@ -1,8 +1,25 @@
+import os
+
 import pygame
 
 
+def load_image(name, colorkey=None):
+    fullname = os.path.join('data', name)
+    image = pygame.image.load(fullname)
+    image = image.convert_alpha()
+
+    if colorkey is not None:
+        if colorkey == -1:
+            colorkey = image.get_at((0, 0))
+        image.set_colorkey(colorkey)
+    else:
+        image = image.convert_alpha()
+    return image
+
+
 def new_coords(coords):
-    if abs(coords[0][0] - coords[1][0]) >= H // 1.2 or abs(coords[0][1] - coords[1][1]) >= W // 1.2:
+    if abs(coords[0][0] - coords[1][0]) >= W // 1.5 or \
+            abs(coords[0][1] - coords[1][1]) >= H // 1.5:
         x_0, y_0 = coords[1]
         x_1, y_1 = coords[2]
     else:
@@ -21,9 +38,25 @@ def new_coords(coords):
         py = H
 
     coords = coords[1:] + [[px, py]]
-    print(coords)
 
     return coords
+
+
+class Circle:
+    def __init__(self, x, y, r, isObstacle):
+        self.isObstacle = isObstacle
+        self.x = x
+        self.y = y
+        self.r = r
+        self.defeated = False
+        self.color = pygame.Color('blue')
+        self.color_def = pygame.Color('green')
+
+    def draw(self, surf):
+        if self.defeated:
+            pygame.draw.circle(surf, self.color_def, (self.x, self.y), self.r)
+        else:
+            pygame.draw.circle(surf, self.color, (self.x, self.y), self.r)
 
 
 class MainObject:
@@ -33,13 +66,17 @@ class MainObject:
         else:
             move = move[0]
         self.move = [_ for _ in move]
-        # self.speed = 10
+        self.len = len(self.move)
 
     def go(self):
         if not self.move[0]:
             return
         self.len = len(self.move)
         self.move = new_coords(self.move)
+
+    def erase(self):
+        self.move = self.move[1:]
+        print(self.move)
 
     def append(self, coord):
         self.move.append(coord)
@@ -55,20 +92,26 @@ class MainObject:
 
 
 pygame.init()
-size = W, H = 500, 500
+size = W, H = 889, 500
 # size = W, H = pygame.display.Info().current_w, pygame.display.Info().current_h
 # screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
 
-screen = pygame.display.set_mode((500, 500))
+# fon = pygame.transform.scale(load_image('fon.jpg'), (W, H))
+
+screen = pygame.display.set_mode((W, H))
 clock = pygame.time.Clock()
 fps = 100
 
 step = 1
 
-main_object_color = pygame.Color('black')
+main_object_color = pygame.Color('#CD5555')
+
+circles = [Circle(100, 100, 20, False), Circle(180, 100, 20, False), Circle(180, 180, 20, False)]
+circles.extend([Circle(200, 200, 20, True),
+                Circle(280, 200, 20, True), Circle(280, 280, 20, True)])
 
 running = True
-isMainObjectCreation = False
+isMainObjectCreation = isGameLost = isGameWon = False
 main_object = MainObject()
 
 while running:
@@ -78,39 +121,66 @@ while running:
             running = False
 
         if event.type == pygame.KEYUP and event.key == 27:
-            screen = pygame.display.set_mode((500, 500))
-            W = H = 500
+            screen = pygame.display.set_mode((889, 500))
+            W = 500
+            H = 889
 
         if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
             main_object.clear()
             main_object.append(list(event.pos))
             isMainObjectCreation = True
+            for i in circles:
+                i.defeated = False
 
         if event.type == pygame.MOUSEBUTTONUP and event.button == 1:
             main_object.append(list(event.pos))
             isMainObjectCreation = False
+            for i in circles:
+                i.defeated = False
 
         if event.type == pygame.MOUSEBUTTONDOWN and event.button == 3:
             main_object.clear()
             main_object.append(None)
+            for i in circles:
+                i.defeated = False
 
         if event.type == pygame.MOUSEMOTION:
             if isMainObjectCreation:
                 main_object.append(list(event.pos))
 
+    for i in circles:
+        try:
+            x1, y1 = main_object.move[-1]
+            if (i.x - x1) ** 2 + (i.y - y1) ** 2 <= i.r ** 2:
+                i.defeated = True
+        except:
+            pass
+        i.draw(screen)
+
     for i in range(0, len(main_object) - 1, step):
         from_coord = main_object.move[i]
         to_coord = main_object.move[i + 1]
-        if abs(from_coord[0] - to_coord[0]) > H // 2 or abs(from_coord[1] - to_coord[1]) > W:
+        if abs(from_coord[0] - to_coord[0]) > W // 2 or abs(from_coord[1] - to_coord[1]) > H:
             continue
         if from_coord[0] and to_coord[0]:
             pygame.draw.line(screen, main_object_color,
                              from_coord, to_coord, 4)
-    if not isMainObjectCreation:
+
+    if any([_.defeated and _.isObstacle for _ in circles]):
+        isGameLost = True
+    elif all([_.defeated and _.defeated != _.isObstacle for _ in circles]):
+        isGameWon = True
+
+    if isGameLost:
+        main_object.erase()
+        if not main_object:
+            isGameLost = False
+    elif isGameWon:
+        pass
+    elif not isMainObjectCreation:
         main_object.go()
 
     clock.tick(fps)
-    # print(*[_ for _ in main_object.move])
 
     pygame.display.flip()
     pygame.display.set_caption("fps: " + str(clock.get_fps()))
